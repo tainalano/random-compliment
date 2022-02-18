1c828a89d87966ba71a230701efe503ba0f17694
Tainá Alano <taialano@gmail.com>
2022-02-18 00:53:13 -0300

    Create the website

---
+++ script.js
@@ -0,0 +1,14 @@
+const complimentList = [
+    "classy", "beautiful", "smart", "superb", "terrific", "hot", "awesome", "genius", "stunning", "wondrous", "perfect", "handsome", "pretty", "lovely", "luxurius", "happy", "nice", "good", "charming", "smoking hot", "delicious", "delicate", "adorable"
+];
+
+const complimentButton = document.getElementById("button")
+const complimentText = document.getElementById("compliment")
+
+function generateNewCompliment() {
+    const newCompliment = complimentList[Math.floor(Math.random() * complimentList.length)];
+    complimentText.innerText = newCompliment
+    console.log(newCompliment)
+}
+
+complimentButton.addEventListener("click", generateNewCompliment)
